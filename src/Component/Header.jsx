7dcc1de4ebcf45609ee83a1c73f48f56529ff0c4
Dawit Birhanu <dawitbirhanu2323@gmail.com>
2026-08-12 10:08:42 +0300

    Update contact link to Telegram and add social icons

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 
 export default function Header() {
@@ -14,7 +15,13 @@ export default function Header() {
                     <li><a href="#projects">Projects</a></li>
                 </ul>
             </nav>
-            <a href="mailto:dawitbirhanu2323@gmail.com" className="cta-nav">Let's Talk <i className="fa-solid fa-arrow-up-right-from-square" style={{ fontSize: "0.75rem" }}></i></a>
+
+            <a href="https://t.me/@zltan7" className="cta-nav">Let's Talk <i className="fa-solid fa-arrow-up-right-from-square" style={{ fontSize: "0.75rem" }}></i></a>
+            <div className="social-links">
+                <a href="https://github.com/DEVA-BIR" className="social-icon" target="_blank"><i className="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/dawit-birhanu-b26a78300?utm_source=share_via&utm_content=profile&utm_medium=member_android" className="social-icon" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
+                <a href="mailto:dawitbirhanu2323@gmail.com" className="social-icon"><i className="fa-solid fa-envelope"></i></a>
+            </div>
         </div>
     </header>
     </div>
