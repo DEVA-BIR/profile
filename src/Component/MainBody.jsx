2984114082578723a44e1e2314b24e8e01f237d6
Dawit Birhanu <dawitbirhanu2323@gmail.com>
2026-09-21 16:32:13 +0300

    Update download link for CV file

--- src/Component/MainBody.jsx
+++ src/Component/MainBody.jsx
@@ -50,8 +50,8 @@ export default function Main() {
                     </p>
             <div className="btn-container">
                 <a
-                            href="/Dawit_Updated.pdf"
-                            download="Dawit_Updated.pdf"
+                            href="/CV last.pdf"
+                            download="CV last.pdf"
                             className="btn btn-primary"
                             aria-label="Download Dawit Birhanu CV"
                         >
